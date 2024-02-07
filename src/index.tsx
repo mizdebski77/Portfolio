7d4c1c5aedf44e7d130c0f48a3e448665af56d0f
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-07 20:37:22 +0100

    Bugs from app.js removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './core/App';
 import reportWebVitals from './core/reportWebVitals';
 
@@ -8,9 +6,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
