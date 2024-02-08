7a30c2404bc082a020dc25c6856e416513fda774
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-08 23:30:16 +0100

    Box to home section added

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,10 +1,27 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import profile from '../common/Images/profile.png';
+import gif from '../common/Images/xd.svg';
 
 export const Home = () => {
     return (
-        <Container sx={{  height: '100vh', display: 'flex', alignItems: 'center' }}>
-            <img src={profile} />
+        <Container sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}>
+            <svg style={{ position: 'absolute', bottom: 0 }} />
+            <Box sx={{
+                maxWidth: '550px',
+                width: '100%',
+                position: "sticky",
+                overflow: "hidden",
+                display: 'flex',
+                background: '#100c04',
+                alignItems: 'center',
+                justifyContent: 'center',
+                boxShadow: '50px -50px 60px #3c4601, -80px 60px 60px #191e00',
+                border: 2,
+                borderColor: 'primary.contrastText',
+                borderRadius: '50%'
+            }}>
+                <img src={profile} />
+            </Box>
 
         </Container>
     );
