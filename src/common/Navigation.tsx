a4e1a2b787abe0c9c977cbeedc2cd887e4e4623d
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-08 16:01:22 +0100

    Unused elements removed

--- src/common/Navigation.tsx
+++ src/common/Navigation.tsx
@@ -1,19 +1,10 @@
-import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import React from 'react';
-import xd from '../icon.png';
 
 export const Navigation = () => {
     return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar
-                position="fixed"
-            >
-                <Toolbar>
-                    <Typography variant="h6" noWrap component="div">
-                        Permanent drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
+        <div>
+
+        </div>
+    );
 };
+
