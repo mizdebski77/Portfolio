53f130ce92e0b271e85e6ace6b598aa8ba3f8dc0
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-08 17:34:48 +0100

    Container to home section added

--- src/common/Navigation.tsx
+++ src/common/Navigation.tsx
@@ -1,18 +1,34 @@
-import JavascriptIcon from '@mui/icons-material/Javascript';
+import LaptopIcon from '@mui/icons-material/Laptop';
 import { AppBar, Box, IconButton, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
 export const Navigation = () => {
     return (
-        <AppBar position='static'>
-            <Toolbar>
-                <IconButton sx={{ color: 'primary.contrastText', p: 0 }} >
-                    <JavascriptIcon sx={{ fontSize: '4rem' }} />
-                </IconButton>
-                <Typography sx={{ fontSize: 28 }} variant='h5' component='span' >
-                    Marcin
-                    <Typography sx={{ color: 'primary.light', fontSize: 28 }} variant='h5' component='span' > Izdebski</Typography>
-                </Typography>
+        <AppBar position='static' sx={{ px: 20, }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <IconButton sx={{ color: 'primary.contrastText', p: 0, }} >
+                        <LaptopIcon sx={{ fontSize: '2.5rem', mr: 2 }} />
+                    </IconButton>
+                    <Typography sx={{ fontSize: 28 }} variant='h5' component='span' >
+                        Marcin
+                        <Typography sx={{ color: 'primary.light', fontSize: 28 }} variant='h5' component='span' > Izdebski</Typography>
+                    </Typography>
+                </Box>
+
+                <div>
+                    <Tabs
+                        sx={{ color: 'primary.contrastText' }}
+                        aria-label="Tabs where selection follows focus"
+                        selectionFollowsFocus
+                    >
+                        <Tab label="Home" sx={{ color: 'primary.contrastText' }} />
+                        <Tab label="Skills" sx={{ color: 'primary.contrastText' }} />
+                        <Tab label="Portfolio" sx={{ color: 'primary.contrastText' }} />
+                        <Tab label="Contact" sx={{ color: 'primary.contrastText' }} />
+                    </Tabs>
+                </div>
+
             </Toolbar>
         </AppBar >
     );
--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,7 +1,12 @@
+import { Container } from "@mui/material";
+import profile from '../common/Images/profile.png';
 
 export const Home = () => {
     return (
-        null
+        <Container sx={{  height: '100vh', display: 'flex', alignItems: 'center' }}>
+            <img src={profile} />
+
+        </Container>
     );
 };
 
