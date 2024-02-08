ced07435c8a142a6cca423aee396a2090f4b732b
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-08 15:51:06 +0100

    Body background color changed

--- src/common/Navigation.tsx
+++ src/common/Navigation.tsx
@@ -1,22 +1,19 @@
-import { Box, Tab, Tabs, Typography } from '@mui/material';
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 import xd from '../icon.png';
 
 export const Navigation = () => {
     return (
-        <Box sx={{ width: '100%', }}>
-            <Box sx={{ borderBottom: 1, py: 0.5, px: 4, borderColor: 'divider', backgroundColor: 'orange', display: 'flex', justifyContent: 'space-between' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-                    <img style={{ maxWidth: '40px', height: 'auto' }} src={xd} alt="Icon" />
-                    <span>Marcin Izdebski</span>
-                </Box>
-
-                <Tabs aria-label="basic tabs example">
-                    <Tab label="Item One" />
-                    <Tab label="Item Two" />
-                    <Tab label="Item Three" />
-                </Tabs>
-            </Box>
+        <Box sx={{ display: 'flex' }}>
+            <AppBar
+                position="fixed"
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div">
+                        Permanent drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </Box>
-    );
+    )
 };
--- src/core/App.tsx
+++ src/core/App.tsx
@@ -3,8 +3,18 @@ import { Navigation } from "../common/Navigation";
 import { Home } from "../features/Home";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { lime, purple } from '@mui/material/colors';
 
 const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "#100c04",
+        }
+      }
+    }
+  }
 });
 
 function App() {
