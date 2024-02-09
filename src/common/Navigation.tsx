525ed19166a8e3465c9c75a62756484a14bb066c
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-09 18:32:34 +0100

    Framer motion installed

--- src/common/Navigation.tsx
+++ src/common/Navigation.tsx
@@ -5,7 +5,7 @@ import { navLinks } from './navLinks';
 
 export const Navigation = () => {
     return (
-        <AppBar position='static' sx={{ px: 20, }}>
+        <AppBar sx={{ position: 'sticky', px: 4, py: 2, zIndex: 1 }}>
             <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                 <Box sx={{ display: 'flex', alignItems: 'center' }}>
                     <IconButton sx={{ color: 'primary.contrastText', p: 0, }} >
@@ -19,12 +19,19 @@ export const Navigation = () => {
 
                 <div>
                     <Tabs
-                        sx={{ color: 'primary.contrastText' }}
+                        sx={{ color: 'primary.contrastText',  }}
                         aria-label="Tabs where selection follows focus"
                         selectionFollowsFocus
                     >
                         {navLinks.map((navLink, index) => (
-                            <Tab label={navLink.text} sx={{ color: 'primary.contrastText' }} />
+                            <Tab
+                                key={index}
+                                label={navLink.text}
+                                sx={{
+                                    color: 'primary.contrastText',
+                                    fontSize: 20
+                                }}
+                            />
                         ))}
                     </Tabs>
                 </div>
