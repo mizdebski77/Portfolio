cb0d8ec6e98c073c126afdb4be38e05c7f107690
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-13 09:40:46 +0100

    Use in view component added

---
+++ src/core/useInView.ts
@@ -0,0 +1,17 @@
+import { useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react"
+
+const useInViewAnimation = (amount: number) => {
+    const ref = useRef(null);
+    const inView = useInView(ref, { amount: amount, once: false });
+    const animation = useAnimation();
+
+    useEffect(() => {
+        if (inView) {
+            animation.start("visible");
+        }
+    }, [animation, inView]);
+
+    return { animation, ref };
+}
+export default useInViewAnimation;
