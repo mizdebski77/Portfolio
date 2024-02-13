361033f09325dc97292e5bdf0a69c0bb55eaeabb
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-13 13:38:18 +0100

    Code and demo url added

---
+++ src/common/Arrays/porfolio.ts
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        id: 1,
+        name: 'Easy-Employ',
+        description:
+            'Page is written in React with TypeScript. During development I worked with experienced figma designers and senior programmers.',
+        tags: [
+            'REACT',
+            'REACT HOOKS',
+            'TYPESCRIPT',
+            'STYLED-COMPONENTS',
+            'GIT',
+            'REACT-HELMET',
+        ],
+        image: 'images/terraeye.jpg',
+        liveUrl: 'mizdebski77.github.io/Easy-Employ/',
+        codeUrl: 'https://github.com/mizdebski77/Easy-Employ',
+        APIlink: undefined,
+    },
+]
--- src/features/Sections/Portfolio.tsx
+++ src/features/Sections/Portfolio.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import { theme } from '../../core/theme';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import { projects } from '../../common/Arrays/porfolio';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
 
 export const Portfolio = () => {
     return (
@@ -32,6 +36,70 @@ export const Portfolio = () => {
                     p: 4,
                 }}
             >
+                {projects.map((project) => (
+                    <Grid2 key={project.id} >
+                        <img src={project.image} />
+                        <Box>
+                            <Box>
+                                <Typography component='span'>
+                                    {project.name}
+                                </Typography>
+                                <Stack direction="row" spacing={2}>
+
+                                    <Link
+                                        href={project.liveUrl}
+                                        target="_blank"
+                                        sx={{
+                                            px: 4,
+                                            py: 1,
+                                            border: 1,
+                                            borderColor: theme.palette.primary.contrastText,
+                                            fontSize: 18,
+                                            color: theme.palette.primary.contrastText,
+                                            borderRadius: 20,
+                                            textDecoration: 'none',
+                                            fontWeight: 'bold',
+                                            transition: "background 0.4s, color 0.4s",
+                                            "&:hover": {
+                                                background: theme.palette.primary.contrastText,
+                                                color: theme.palette.primary.main,
+                                            }
+                                        }}
+                                    >
+                                        Live
+                                    </Link>
+
+                                    <Link
+                                        href={project.codeUrl}
+                                        target="_blank"
+                                        sx={{
+                                            px: 4,
+                                            py: 1,
+                                            background: theme.palette.primary.contrastText,
+                                            color: theme.palette.primary.main,
+                                            fontSize: 18,
+                                            borderRadius: 20,
+                                            textDecoration: 'none',
+                                            transition: "background 0.4s, color 0.4s",
+                                            border: 1,
+                                            "&:hover": {
+                                                color: theme.palette.primary.contrastText,
+                                                background: theme.palette.primary.main,
+                                            }
+                                        }}
+                                    >
+                                        Code
+                                    </Link>
+                                </Stack>
+                            </Box>
+
+                            <Typography component='span'>
+                                {project.description}
+                            </Typography>
+                        </Box>
+                    </Grid2>
+                ))}
+
 
 
             </Container >
