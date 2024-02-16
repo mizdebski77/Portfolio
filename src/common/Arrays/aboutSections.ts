53d82a3d5318d901d6e7e876a6ec7e09ed516ea0
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-16 15:40:46 +0100

    Paper to about me section added

---
+++ src/common/Arrays/aboutSections.ts
@@ -0,0 +1,16 @@
+export const aboutInformations = [
+    {
+        id: 1,
+        title: 'Easy-Employ',
+    },
+
+    {
+        id: 1,
+        title: 'Easy-Employ',
+    },
+
+    {
+        id: 1,
+        title: 'Easy-Employ',
+    },
+]
--- src/features/Sections/AboutMe.tsx
+++ src/features/Sections/AboutMe.tsx
@@ -1,8 +1,8 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, Stack, Paper } from '@mui/material';
 import { theme } from '../../core/theme';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-
+import { aboutInformations } from '../../common/Arrays/aboutSections';
 
 export const About = () => {
 
@@ -29,14 +29,35 @@ export const About = () => {
                 About Me
             </Typography>
 
-            <Container
-                maxWidth='lg'
-                sx={{
-                    p: 4,
-                }}
+            <Stack
+                margin='auto'
+                direction='row'
+                spacing={4}
+                maxWidth={1500}
+                justifyContent='space-evenly'
+                padding='40px'
             >
-
-            </Container >
+                {aboutInformations.map((section) => (
+                    <Paper
+                        key={section.id}
+                        sx={{
+                            background: 'rgba(255, 255, 255, 0.2)',
+                            width: '100%',
+                            p: 4,
+                            borderRadius: 4
+                        }}
+                    >
+                        <Typography
+                            variant='h2'
+                            sx={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 28,
+                            }}
+                        >{section.title}</Typography>
+                    </Paper>
+                ))}
+            </Stack >
 
         </Box >
     );
