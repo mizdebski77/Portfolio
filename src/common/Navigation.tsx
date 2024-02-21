ff4ae9936503551ef2a313e453f632bd03b4b3e2
Marcin Izdebski <mizdebski123@gmail.com>
2024-02-21 13:27:07 +0100

    Actve link style added

--- src/common/Navigation.tsx
+++ src/common/Navigation.tsx
@@ -1,8 +1,8 @@
 import LaptopIcon from '@mui/icons-material/Laptop';
-import { AppBar, Box, IconButton, Tabs, Toolbar, Typography, Stack } from '@mui/material';
+import { AppBar, Box, IconButton, Tabs, Toolbar, Typography, Stack, SvgIcon } from '@mui/material';
 import { navLinks } from './Arrays/navLinks';
 import { NavLink } from './Navigation/StyledNavbar';
-
+import ComputerIcon from '@mui/icons-material/Computer';
 
 export const Navigation = () => {
     console.log();
@@ -12,9 +12,15 @@ export const Navigation = () => {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="fixed" sx={{ zIndex: 10 }}>
                 <Toolbar sx={{ display: 'grid', gridTemplateColumns: '1.5fr 1fr' }}>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
+                    <Stack direction='row' alignItems='center' spacing={2}>
+                        <SvgIcon sx={{ width: 40, height: 40 }}>
+                            <ComputerIcon />
+                        </SvgIcon>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontSize: 28, letterSpacing: 0 }}>
+                            Marcin Izdebski
+                        </Typography>
+                    </Stack>
+
                     <Stack direction='row' justifyContent='space-evenly'>
                         {navLinks.map((link, index) => (
                             <NavLink
--- src/common/Navigation/StyledNavbar.ts
+++ src/common/Navigation/StyledNavbar.ts
@@ -1,15 +1,17 @@
 import { Link } from "react-scroll";
 import { styled } from "styled-components";
 
+const activeLink = "active";
 
 export const NavLink = styled(Link)`
-    font-size: 20px;
-    transition: 0.3s;
-    padding: 8px 20px;
+   font-size: 20px;
+   transition: 0.4s;
 
-     &:hover {
-        color: white;
-     }
+   &:hover {
+      color:white
+   }
 
-
+   &.${activeLink} {
+      border-bottom: 1px solid #bcdb02;
+   }
 `;
